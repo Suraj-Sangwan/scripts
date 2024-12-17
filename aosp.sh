7ae26d62dc5644c7e1a610c0ab663ea1f24d0f80
Suraj Sangwan <76832120+Suraj-Sangwan@users.noreply.github.com>
2024-12-17 19:26:14 +0530

    Create aosp.sh

---
+++ aosp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+# remove device tree
+rm -rf device/oneplus/fajita
+rm -rf device/oneplus/sdm845-common
+rm -rf kernel/oneplus/sdm845
+rm -rf vendor/oneplus/fajita
+rm -rf hardware/oneplus
+# Initialize ROM manifest
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+# Clone local_manifests repository
+git clone https://github.com/Suraj-Sangwan/local_manifests --depth 1 -b aosp .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+# Sync the repositories
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout' 
+echo "============SYNC DONE================"
+# Set up the build environment
+source build/envsetup.sh
+export TARGET_RELEASE=ap2a
+export ALLOW_MISSING_DEPENDENCIES=true
+echo "====== Envsetup Done ======="
+# Choose the target device
+lunch aosp_fajita-ap2a-userdebug
+mka installclean
+echo "======LUNCH DONE======="
+# Build the ROM (use mka bacon for a full build)
+mka bacon
