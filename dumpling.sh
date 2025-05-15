65ffa57873b83a3fa7393221430ad2e1bedcbde4
Suraj Sangwan <76832120+Suraj-Sangwan@users.noreply.github.com>
2025-05-15 17:40:48 +0530

    Update for EvoX

--- dumpling.sh
+++ dumpling.sh
@@ -8,15 +8,15 @@ rm -rf vendor/oneplus/dumpling
 rm -rf vendor/oneplus/msm8998-common
 rm -rf hardware/oneplus
 # Initialize ROM manifest
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs --depth=1
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 # Clone local_manifests repository
-git clone https://github.com/Suraj-Sangwan/local_manifests --depth 1 -b dumpling .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+#git clone https://github.com/Suraj-Sangwan/local_manifests --depth 1 -b dumpling .repo/local_manifests
+#echo "============================"
+#echo "Local manifest clone success"
+#echo "============================"
 # Sync the repositories
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
@@ -26,12 +26,12 @@ fi
 repo forall -c 'git lfs install && git lfs pull && git lfs checkout' 
 echo "============SYNC DONE================"
 # Set up the build environment
-source build/envsetup.sh
+. build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 echo "====== Envsetup Done ======="
 # Choose the target device
-riseup dumpling userdebug
+lunch lineage_dumpling-bp1a-userdebug
 echo "======LUNCH DONE======="
 # Build the ROM (use mka bacon for a full build)
-rise b
+m evolution
