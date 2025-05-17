75f28e4893a14cd5f449254fe2ebc714aa719e1a
Suraj Sangwan <76832120+Suraj-Sangwan@users.noreply.github.com>
2025-05-17 16:57:20 +0530

    Script for fajita evox build

---
+++ fajita.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+# remove device tree
+rm -rf device/oneplus/fajita
+rm -rf device/oneplus/sdm845-common
+rm -rf kernel/oneplus/sdm845
+rm -rf vendor/oneplus/fajita
+rm -rf vendor/oneplus/sdm845-common
+rm -rf hardware/oneplus
+# Initialize ROM manifest
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+# Clone local_manifests repository
+git clone https://github.com/Suraj-Sangwan/local_manifests --depth 1 -b fajita .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+# Sync the repositories
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout' 
+echo "============SYNC DONE================"
+# Set up the build environment
+. build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+echo "====== Envsetup Done ======="
+# Choose the target device
+lunch lineage_fajita-bp1a-userdebug
+echo "======LUNCH DONE======="
+# Build the ROM (use mka bacon for a full build)
+m evolution
